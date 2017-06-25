6abfee0406249a42aaf98f10779c181de1e70f76
David <barrard@gmail.com>
2017-06-25 11:54:51 -1000

    Lots of changes, added user lsit, lots of code refactor, decuppling, modular....

--- build/bld.js
+++ build/bld.js
@@ -567,6 +567,76 @@ var Chatter = function (coreModule) {
 	return coreModule;
 }(Chatter || {});
 
+//chatter functions
+var Chatter = function (module) {
+	'use strict';
+
+	console.log('Chatter functions module is here');
+
+	var chattersList = [];
+
+	function createUserList(socketObjList) {
+		socketObjList.forEach(function (socketObj) {
+			chattersList.push(socketObj);
+		});
+		Chatter.createUsersTab();
+	}
+
+	function insertUserInList(socketDataObj) {
+		chattersList.push(socketDataObj);
+		console.log('new chatter added to list');
+		console.log(chattersList);
+		console.log(getChatterList());
+		if (Chatter.queryShadowDom('._userListBox')) {
+			Chatter.renderUserList();
+		}
+	}
+	function insertNameInSocketObject(socketId, socketName) {
+		console.log(socketId);
+		console.log(socketName);
+		var socketArray = getChatterList();
+		var pos = socketArray.map(function (i) {
+			return i.socketid;
+		}).indexOf(socketId);
+		console.log(pos);
+		console.log(socketArray[pos]);
+		socketArray[pos]['username'] = socketName;
+	}
+	function removeSocketObjInList(id) {
+		var socketArray = getChatterList();
+
+		var pos = socketArray.map(function (i) {
+			return i.socketId;
+		}).indexOf(id);
+		console.log(pos + ' is not negative???');
+
+		if (pos > -1) {
+			console.log('remove this socket ' + id + ' from socketArray');
+			socketArray.splice(pos, 1);
+		} else {
+			console.log('this socket is lost...? ' + id);
+		}
+	}
+	function getChatterList() {
+		return chattersList;
+	}
+	function ShowChattersList() {
+		console.log('show the list! of Chaters'
+		// $Util.queryShadowDom('')
+
+		);
+	}
+
+	module.createUserList = createUserList;
+	module.ShowChattersList = ShowChattersList;
+	module.insertUserInList = insertUserInList;
+	module.getChatterList = getChatterList;
+	module.insertNameInSocketObject = insertNameInSocketObject;
+	module.removeSocketObjInList = removeSocketObjInList;
+
+	return module;
+}(Chatter || {});
+
 var Chatter = function (module) {
 	'use strict';
 
@@ -650,90 +720,6 @@ var Chatter = function (module) {
 	module.initSocket = initSocket;
 
 	module.joinDomainChat = joinDomainChat;
-
-	return module;
-}(Chatter || {}
-
-//admin
-);var Chatter = function (module) {
-	console.log('adminChatter is here');
-	window.onload = function () {
-		console.log('loaded');
-	};
-
-	function verifyAccount() {}
-
-	module.verifyAccount = verifyAccount;
-
-	return module;
-}(Chatter || {}
-
-//chatter functions
-);var Chatter = function (module) {
-	'use strict';
-
-	console.log('Chatter functions module is here');
-
-	var chattersList = [];
-
-	function createUserList(socketObjList) {
-		socketObjList.forEach(function (socketObj) {
-			chattersList.push(socketObj);
-		});
-		Chatter.createUsersTab();
-	}
-
-	function insertUserInList(socketDataObj) {
-		chattersList.push(socketDataObj);
-		console.log('new chatter added to list');
-		console.log(chattersList);
-		console.log(getChatterList());
-		if (Chatter.queryShadowDom('._userListBox')) {
-			Chatter.renderUserList();
-		}
-	}
-	function insertNameInSocketObject(socketId, socketName) {
-		console.log(socketId);
-		console.log(socketName);
-		var socketArray = getChatterList();
-		var pos = socketArray.map(function (i) {
-			return i.socketid;
-		}).indexOf(socketId);
-		console.log(pos);
-		console.log(socketArray[pos]);
-		socketArray[pos]['username'] = socketName;
-	}
-	function removeSocketObjInList(id) {
-		var socketArray = getChatterList();
-
-		var pos = socketArray.map(function (i) {
-			return i.socketId;
-		}).indexOf(id);
-		console.log(pos + ' is not negative???');
-
-		if (pos > -1) {
-			console.log('remove this socket ' + id + ' from socketArray');
-			socketArray.splice(pos, 1);
-		} else {
-			console.log('this socket is lost...? ' + id);
-		}
-	}
-	function getChatterList() {
-		return chattersList;
-	}
-	function ShowChattersList() {
-		console.log('show the list! of Chaters'
-		// $Util.queryShadowDom('')
-
-		);
-	}
-
-	module.createUserList = createUserList;
-	module.ShowChattersList = ShowChattersList;
-	module.insertUserInList = insertUserInList;
-	module.getChatterList = getChatterList;
-	module.insertNameInSocketObject = insertNameInSocketObject;
-	module.removeSocketObjInList = removeSocketObjInList;
 
 	return module;
 }(Chatter || {});
@@ -867,6 +853,19 @@ var Chatter = function (module) {
 	module.createRoomTab = createRoomTab;
 	module.renderUserList = renderUserList;
 	module.createUsersTab = createUsersTab;
+
+	return module;
+}(Chatter || {}
+//admin
+);var Chatter = function (module) {
+	console.log('adminChatter is here');
+	window.onload = function () {
+		console.log('loaded');
+	};
+
+	function verifyAccount() {}
+
+	module.verifyAccount = verifyAccount;
 
 	return module;
 }(Chatter || {});
--- client/chatBuild.js
+++ client/chatBuild.js
@@ -881,6 +881,79 @@ var Chatter = (function(coreModule) {
 
 }(Chatter || {}))
 
+//chatter functions
+var Chatter = (function(module){
+	'use strict'
+	console.log('Chatter functions module is here')
+
+	var chattersList = []
+
+	function createUserList(socketObjList){
+		socketObjList.forEach(function(socketObj){
+			chattersList.push(socketObj)
+		})
+		Chatter.createUsersTab()
+	}
+
+	function insertUserInList(socketDataObj){
+		chattersList.push(socketDataObj)
+		console.log('new chatter added to list')
+		console.log(chattersList)
+		console.log(getChatterList())
+		if(Chatter.queryShadowDom('._userListBox')){
+			Chatter.renderUserList()
+
+		}
+
+
+	}
+	function insertNameInSocketObject(socketId, socketName){
+		console.log(socketId)
+		console.log(socketName)
+		var socketArray = getChatterList()
+		var pos = socketArray.map(function(i) { return i.socketid; }).indexOf(socketId);
+		console.log(pos)
+		console.log(socketArray[pos])
+		socketArray[pos]['username'] = socketName
+
+
+	}
+	function removeSocketObjInList(id){
+		var socketArray = getChatterList()
+
+		var pos = socketArray.map(function(i) { return i.socketId; }).indexOf(id);
+		console.log(pos+' is not negative???')
+
+
+		if (pos > -1) {
+			console.log('remove this socket ' + id + ' from socketArray')
+			socketArray.splice(pos, 1)
+		} else {
+			console.log('this socket is lost...? ' + id)
+		}
+
+	}
+	function getChatterList(){
+		return chattersList
+	}
+	function ShowChattersList(){
+		console.log('show the list! of Chaters')
+		// $Util.queryShadowDom('')
+		
+	}
+
+
+	module.createUserList = createUserList
+	module.ShowChattersList = ShowChattersList
+	module.insertUserInList = insertUserInList
+	module.getChatterList = getChatterList
+	module.insertNameInSocketObject=insertNameInSocketObject
+	module.removeSocketObjInList = removeSocketObjInList
+
+	return module
+})(Chatter || {})
+
+
 var Chatter = (function(module){
 	'use strict'
 	var queryShadowDom = Chatter.queryShadowDom
@@ -993,101 +1066,6 @@ var Chatter = (function(module){
 })(Chatter || {})
 
 
-
-
-//admin
-var Chatter = (function(module){
-	console.log('adminChatter is here')
-	window.onload= function(){
-		console.log('loaded')
-	}
-
-	function verifyAccount(){
-		
-	}
-
-
-
-	module.verifyAccount = verifyAccount
-
-
-return module
-
-
-})(Chatter || {})
-
-
-//chatter functions
-var Chatter = (function(module){
-	'use strict'
-	console.log('Chatter functions module is here')
-
-	var chattersList = []
-
-	function createUserList(socketObjList){
-		socketObjList.forEach(function(socketObj){
-			chattersList.push(socketObj)
-		})
-		Chatter.createUsersTab()
-	}
-
-	function insertUserInList(socketDataObj){
-		chattersList.push(socketDataObj)
-		console.log('new chatter added to list')
-		console.log(chattersList)
-		console.log(getChatterList())
-		if(Chatter.queryShadowDom('._userListBox')){
-			Chatter.renderUserList()
-
-		}
-
-
-	}
-	function insertNameInSocketObject(socketId, socketName){
-		console.log(socketId)
-		console.log(socketName)
-		var socketArray = getChatterList()
-		var pos = socketArray.map(function(i) { return i.socketid; }).indexOf(socketId);
-		console.log(pos)
-		console.log(socketArray[pos])
-		socketArray[pos]['username'] = socketName
-
-
-	}
-	function removeSocketObjInList(id){
-		var socketArray = getChatterList()
-
-		var pos = socketArray.map(function(i) { return i.socketId; }).indexOf(id);
-		console.log(pos+' is not negative???')
-
-
-		if (pos > -1) {
-			console.log('remove this socket ' + id + ' from socketArray')
-			socketArray.splice(pos, 1)
-		} else {
-			console.log('this socket is lost...? ' + id)
-		}
-
-	}
-	function getChatterList(){
-		return chattersList
-	}
-	function ShowChattersList(){
-		console.log('show the list! of Chaters')
-		// $Util.queryShadowDom('')
-		
-	}
-
-
-	module.createUserList = createUserList
-	module.ShowChattersList = ShowChattersList
-	module.insertUserInList = insertUserInList
-	module.getChatterList = getChatterList
-	module.insertNameInSocketObject=insertNameInSocketObject
-	module.removeSocketObjInList = removeSocketObjInList
-
-	return module
-})(Chatter || {})
 
 
 var Chatter = (function(module){
@@ -1241,6 +1219,28 @@ function closeUserList(e){
 	return module
 	
 })(Chatter || {})
+//admin
+var Chatter = (function(module){
+	console.log('adminChatter is here')
+	window.onload= function(){
+		console.log('loaded')
+	}
+
+	function verifyAccount(){
+		
+	}
+
+
+
+	module.verifyAccount = verifyAccount
+
+
+return module
+
+
+})(Chatter || {})
+
+
 
   document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
